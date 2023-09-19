204fc50a5c538aa7774b8dad66d8345cfa4787eb
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-09-19 14:10:05 +0200

    test

--- lezione22/typescript/hellow.js
+++ lezione22/typescript/hellow.js
@@ -1,2 +1,24 @@
-var message = 'Hello, World!';
+var message = 'Ciao, Mondo!';
 console.log(message);
+function addNumbers(a, b) {
+    return a + b;
+}
+var sum = addNumbers(10, 15);
+var myName = "Paolo";
+myName = myName + " Musa";
+console.log('Sum of the two numbers is: ' + sum);
+var nameX = 'Danny';
+var arr = [1, 3, 5, 7];
+arr.pop();
+console.log(arr); // [1, 3, 5] - il metodo di array ha cambiato l'array
+var id = 5;
+var firstname = 'danny';
+var hasDog = true;
+var unit; // Dichiara la variabile senza assegnare un valore
+unit = 5;
+/*
+npx tsc index.ts --outfile file-name.js
+
+npx tsc hellow.ts -w
+
+*/ 
--- lezione22/typescript/hellow.ts
+++ lezione22/typescript/hellow.ts
@@ -1,2 +1,36 @@
-let message: string = 'Hello, World!';
+let message: string = 'Ciao, Mondo!';
 console.log(message);
+
+function addNumbers(a: number, b: number) { 
+    return a + b; 
+} 
+
+let sum: number = addNumbers(10, 15) 
+
+let myName: string ="Paolo";
+myName = myName + " Musa";
+
+console.log('Sum of the two numbers is: ' +sum); 
+
+let nameX = 'Danny';
+
+let arr = [1, 3, 5, 7];
+arr.pop();
+console.log(arr); // [1, 3, 5] - il metodo di array ha cambiato l'array
+
+
+let id: number = 5;
+let firstname: string = 'danny';
+let hasDog: boolean = true;
+
+let unit: number; // Dichiara la variabile senza assegnare un valore
+
+unit = 5;
+
+
+/*
+npx tsc index.ts --outfile file-name.js
+
+npx tsc hellow.ts -w
+
+*/
