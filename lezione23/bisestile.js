ab186033e283d3b00b607eab4f90a3b7e64a6c2e
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-09-20 13:12:47 +0200

    bisestile

---
+++ lezione23/bisestile.js
@@ -0,0 +1,8 @@
+//calcolare se un anno è bisestile
+/*
+
+Algoritmo anno bisestile, controllare se un anno è bisestile
+Per vedere se un anno è bisestile dobbiamo dividerlo per 400; se è divisibile allora è sicuramente un anno bisestile; altrimenti saranno bisestili solo quegli anni che si possono dividere per 4 ma non per 100. Esempi di anni bisestili sono: 1600, 1604, 1608 fino al 1696. Il 1700 non è bisestile.
+*/
+var anni;
+anni = [1980, 2000, 1900, 1650, 1931, 2024];
