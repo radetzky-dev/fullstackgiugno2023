84c81bfa8a8dd75c23ff81f756b43cbfd8e8fd4d
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-09-20 11:06:40 +0200

    last

--- lezione23/helloworld.js
+++ lezione23/helloworld.js
@@ -1,8 +1,8 @@
-var message = 'Ciao, Mondo!';
+var message = "Ciao, Mondo!";
 var eta = 18;
-var cities = ['Milano', 'Roma'];
+var cities = ["Milano", "Roma"];
 var numbers = [7, 21, 46];
-var languages = ['Javascript', 'Typescript'];
+var languages = ["Javascript", "Typescript"];
 console.log(message);
 function myLog(s) {
     console.log("Scrivo nel log " + s);
@@ -14,25 +14,25 @@ var risultato;
 risultato = calcola(7, 5);
 console.log("risultato " + risultato);
 var a = 11;
-if (typeof a === 'number') {
+if (typeof a === "number") {
     console.log("numero " + a);
 }
-console.log(typeof (risultato));
+console.log(typeof risultato);
 myLog("ciao");
 console.log("Risultato " + risultato);
 var qualsiasi = "10";
-console.log(typeof (qualsiasi));
+console.log(typeof qualsiasi);
 qualsiasi = 10;
 qualsiasi = [7, 21, 46];
-console.log(typeof (qualsiasi));
+console.log(typeof qualsiasi);
 console.log(qualsiasi[0]);
 // Definisce una funzione chiamata circle che ottiene una variabile di tipo number e ritorna un valore di tipo string
 function circle(diam) {
-    return 'The circumference is ' + Math.PI * diam;
+    return "The circumference is " + Math.PI * diam;
 }
 console.log(circle(10));
 var circle2 = function (diam, raggio) {
-    return 'Raggio ' + raggio + ' circonferenza è ' + Math.PI * diam;
+    return "Raggio " + raggio + " circonferenza è " + Math.PI * diam;
 };
 console.log(circle2(10, 5));
 console.log("***********");
@@ -40,10 +40,32 @@ var add = function (a, b, c) {
     console.log(c);
     return a + b;
 };
-console.log(add(5, 4, 'I could pass a number, string, or nothing here!'));
+console.log(add(5, 4, "I could pass a number, string, or nothing here!"));
 console.log(add(3, 2));
-var form = document.getElementById('signup-form');
-console.log(form.method); // post
+//const form = document.getElementById("signup-form") as HTMLFormElement;
+//console.log(form.method); // post
+var Person = /** @class */ (function () {
+    function Person(name, c, pets, age) {
+        this.name = name;
+        this.isCool = c;
+        this.pets = pets;
+        this.age = age;
+    }
+    Person.prototype.sayHello = function () {
+        return "Hi, my name is ".concat(this.name, " and I have ").concat(this.pets, " pets");
+    };
+    Person.prototype.myAge = function () {
+        return this.age;
+    };
+    return Person;
+}());
+var person1 = new Person("Danny", false, 1, 34);
+var person2 = new Person("Sarah", true, 6, 28); // ERRORE: Argomento di tipo 'string' non è assegnabile al parametro di tipo 'boolean'.
+console.log(person1.sayHello());
+console.log(person2.sayHello());
+console.log("Ho " + person2.myAge() + " anni");
+var People = [person1, person2];
+console.log(People[0].myAge());
 /*
 Installazione nella cartella
 npm install typescript --save-dev
@@ -53,4 +75,4 @@ npx tsc index.ts
 
 Compilazione "perpetua"
 npx tsc index.ts -w
-*/ 
+*/
--- lezione23/helloworld.ts
+++ lezione23/helloworld.ts
@@ -1,79 +1,105 @@
-let message: string = 'Ciao, Mondo!';
+let message: string = "Ciao, Mondo!";
 
 let eta: number = 18;
 
-let cities: string[] = ['Milano', 'Roma'];
-let numbers: number[] = [7,21,46];
+let cities: string[] = ["Milano", "Roma"];
+let numbers: number[] = [7, 21, 46];
 
-let languages: Array<string> = ['Javascript','Typescript'];
+let languages: Array<string> = ["Javascript", "Typescript"];
 
 console.log(message);
 
-function myLog(s: string) : void
-{
-    console.log ("Scrivo nel log "+s);
-  
+function myLog(s: string): void {
+  console.log("Scrivo nel log " + s);
 }
 
-
-function calcola(x: number, y: number) 
-{
-    return x+y;
+function calcola(x: number, y: number) {
+  return x + y;
 }
 
 let risultato: number;
 
-  risultato = calcola(7,5);
-  console.log ("risultato "+ risultato);
+risultato = calcola(7, 5);
+console.log("risultato " + risultato);
 
+const a: unknown = 11;
 
-  const a: unknown = 11;
-
-if (typeof a === 'number') {
- console.log ("numero "+a)
+if (typeof a === "number") {
+  console.log("numero " + a);
 }
 
-console.log (typeof(risultato));
+console.log(typeof risultato);
 myLog("ciao");
 
-console.log ("Risultato "+risultato);
+console.log("Risultato " + risultato);
 
-let qualsiasi : any = "10";
-console.log (typeof(qualsiasi));
+let qualsiasi: any = "10";
+console.log(typeof qualsiasi);
 qualsiasi = 10;
-qualsiasi = [7,21,46];
+qualsiasi = [7, 21, 46];
 
-console.log (typeof(qualsiasi));
-console.log (qualsiasi[0]);
-
+console.log(typeof qualsiasi);
+console.log(qualsiasi[0]);
 
 // Definisce una funzione chiamata circle che ottiene una variabile di tipo number e ritorna un valore di tipo string
 function circle(diam: number): string {
-    return 'The circumference is ' + Math.PI * diam;
+  return "The circumference is " + Math.PI * diam;
+}
+
+console.log(circle(10));
+
+const circle2 = (diam: number, raggio: number): string => {
+  return "Raggio " + raggio + " circonferenza è " + Math.PI * diam;
+};
+
+console.log(circle2(10, 5));
+
+console.log("***********");
+
+const add = (a: number, b: number, c?: number | string) => {
+  console.log(c);
+
+  return a + b;
+};
+
+console.log(add(5, 4, "I could pass a number, string, or nothing here!"));
+console.log(add(3, 2));
+
+//const form = document.getElementById("signup-form") as HTMLFormElement;
+//console.log(form.method); // post
+
+class Person {
+  name: string;
+  isCool: boolean;
+  pets: number;
+  age: number;
+
+  constructor(name: string, c: boolean, pets: number, age: number) {
+    this.name = name;
+    this.isCool = c;
+    this.pets = pets;
+    this.age = age;
   }
-  
-  console.log(circle(10)); 
 
-  const circle2 = (diam: number, raggio: number): string => {
-    return 'Raggio '+ raggio+ ' circonferenza è ' + Math.PI * diam;
-  };
-  
-  console.log(circle2(10,5));
+  sayHello(): string {
+    return `Hi, my name is ${this.name} and I have ${this.pets} pets`;
+  }
 
-  console.log ("***********");
+  myAge(): number {
+    return this.age;
+  }
+}
 
-  const add = (a: number, b: number, c?: number | string) => {
-    console.log(c);
-  
-    return a + b;
-  };
-  
-  console.log(add(5, 4, 'I could pass a number, string, or nothing here!'));
-  console.log(add(3, 2));
+const person1 = new Person("Danny", false, 1,34);
+const person2 = new Person("Sarah", true, 6,28); // ERRORE: Argomento di tipo 'string' non è assegnabile al parametro di tipo 'boolean'.
 
-  const form = document.getElementById('signup-form') as HTMLFormElement;
-  console.log(form.method); // post
+console.log(person1.sayHello());
+console.log(person2.sayHello());
+console.log ("Ho "+person2.myAge() +" anni");
 
+let People: Person[] = [person1, person2];
+
+console.log (People[0].myAge());
 
 /*
 Installazione nella cartella
