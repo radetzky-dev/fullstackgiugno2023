d01d847f0d8a4ecc40e5a35b13a9eea641eec741
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-09-20 10:04:29 +0200

    void

--- lezione23/helloworld.js
+++ lezione23/helloworld.js
@@ -1,2 +1,29 @@
 var message = 'Ciao, Mondo!';
+var eta = 18;
+var cities = ['Milano', 'Roma'];
+var numbers = [7, 21, 46];
+var languages = ['Javascript', 'Typescript'];
 console.log(message);
+function myLog(s) {
+    console.log("Scrivo nel log " + s);
+    var result = calcola(9, 4);
+    console.log("Result " + result);
+}
+function calcola(x, y) {
+    return x + y;
+}
+var risultato = true;
+risultato = calcola(7, 5);
+console.log(typeof (risultato));
+myLog("ciao");
+console.log("Risultato " + risultato);
+/*
+Installazione nella cartella
+npm install typescript --save-dev
+
+Compilazione singola
+npx tsc index.ts
+
+Compilazione "perpetua"
+npx tsc index.ts -w
+*/ 
--- lezione23/helloworld.ts
+++ lezione23/helloworld.ts
@@ -1,5 +1,38 @@
 let message: string = 'Ciao, Mondo!';
+
+let eta: number = 18;
+
+let cities: string[] = ['Milano', 'Roma'];
+let numbers: number[] = [7,21,46];
+
+let languages: Array<string> = ['Javascript','Typescript'];
+
 console.log(message);
+
+function myLog(s: string) : void
+{
+    console.log ("Scrivo nel log "+s);
+    let result: number = calcola (9,4);
+    console.log ("Result "+result);
+}
+
+
+function calcola(x: number, y: number) : number
+{
+    return x+y;
+}
+
+let risultato: boolean = true;
+
+risultato = calcola(7,5);
+
+console.log (typeof(risultato));
+myLog("ciao");
+
+console.log ("Risultato "+risultato);
+
+
+
 
 /*
 Installazione nella cartella
