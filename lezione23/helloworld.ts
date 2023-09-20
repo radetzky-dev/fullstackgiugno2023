719022d39c2203ab8f97782e9d2d13c6c9984f90
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-09-20 09:46:21 +0200

    istruzioni

--- lezione23/helloworld.ts
+++ lezione23/helloworld.ts
@@ -1,2 +1,13 @@
 let message: string = 'Ciao, Mondo!';
 console.log(message);
+
+/*
+Installazione nella cartella
+npm install typescript --save-dev
+
+Compilazione singola
+npx tsc index.ts
+
+Compilazione "perpetua"
+npx tsc index.ts -w
+*/
