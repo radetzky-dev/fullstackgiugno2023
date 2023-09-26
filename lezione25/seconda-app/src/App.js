48aad2b1eb6dc5c01d4094880122e0a334d00296
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-09-26 10:15:12 +0200

    seconda app

--- lezione25/seconda-app/src/App.js
+++ lezione25/seconda-app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  let demo = React.createElement(
+    "div",
+    { style: { color: "green" } },
+    React.createElement("h1", { className: "title" }, "To do list"),
+    React.createElement(
+      "ul",
+      null,
+      React.createElement("li", null, "Prima riga"),
+      React.createElement("li", null, "Seconda riga")
+    )
   );
+
+  ReactDOM.render(demo, document.getElementById("root"));
 }
 
 export default App;
