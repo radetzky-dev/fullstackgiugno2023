51a830d34c9e6473489f58df9f18f68dafd58241
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-09-26 10:32:24 +0200

    react

--- lezione25/seconda-app/src/App.js
+++ lezione25/seconda-app/src/App.js
@@ -3,16 +3,32 @@ import "./App.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
+function Populate(myList)
+{
+  return myList.map(i => React.createElement("li", null, i));
+}
+
+
 function App() {
+
+  let myList = ["aTtività 1","atTività 2","attività 3","attività 4", "attività 5"];
+  let myList2 = ["spesa 1","speSa 2","attività 3","attività 4", "attività 5"];
+
   let demo = React.createElement(
     "div",
     { style: { color: "green" } },
     React.createElement("h1", { className: "title" }, "To do list"),
+    React.createElement("h1", { className: "title" }, "Questo è un altro titolo"),
     React.createElement(
       "ul",
       null,
-      React.createElement("li", null, "Prima riga"),
-      React.createElement("li", null, "Seconda riga")
+      //myList.map(i => React.createElement("li", null, i))
+      Populate(myList)
+    ),
+    React.createElement(
+      "ul",
+      null,
+      Populate(myList2)
     )
   );
 
